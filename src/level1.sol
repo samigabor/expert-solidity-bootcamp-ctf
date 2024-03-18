237f09f5b3c900d736dfc42dcccb716c668a2746
samigabor <samoila.gabor@yahoo.com>
2024-03-18 15:21:59 +0200

    level 1 & 2 optimization

--- src/level1.sol
+++ src/level1.sol
@@ -4,9 +4,11 @@ pragma solidity 0.8.17;
 contract Level1Template {
 
     function solution(uint256[2][3] calldata x, uint256[2][3] calldata y) external pure returns (uint256[2][3] memory finalArray) {
-        for (uint256 i = 0; i < x.length; i++) {
-            for (uint256 j = 0; j < x[i].length; j++) {
-                finalArray[i][j] = x[i][j] + y[i][j];
+        unchecked { // Disable overflow checks since we are sure the arrays are of length 2 & 3
+            for (uint256 i = 0; i < 3; i++) { // x.length
+                for (uint256 j = 0; j < 2; j++) { // x[i].length
+                    finalArray[i][j] = x[i][j] + y[i][j];
+                }
             }
         }
     }
--- src/level2.sol
+++ src/level2.sol
@@ -4,14 +4,15 @@ pragma solidity 0.8.17;
 contract Level2Template {
     // Bubble sort algorithm
     function solution(uint256[10] calldata unsortedArray) external pure returns (uint256[10] memory sortedArray) {
-        uint256 n = unsortedArray.length;
         sortedArray = unsortedArray;
 
-        for (uint256 i = 0; i < n - 1; i++) {
-            for (uint256 j = 0; j < n - i - 1; j++) {
-                if (sortedArray[j] > sortedArray[j + 1]) {
-                    // Swap elements
-                    (sortedArray[j], sortedArray[j + 1]) = (sortedArray[j + 1], sortedArray[j]);
+        unchecked { // Disable overflow checks since we are sure the array is of length 10
+            for (uint256 i = 0; i < 9; i++) { // unsortedArray.length - 1
+                for (uint256 j = 0; j < 9 - i; j++) { // unsortedArray.length - i - 1
+                    if (sortedArray[j] > sortedArray[j + 1]) {
+                        // Swap elements
+                        (sortedArray[j], sortedArray[j + 1]) = (sortedArray[j + 1], sortedArray[j]);
+                    }
                 }
             }
         }
